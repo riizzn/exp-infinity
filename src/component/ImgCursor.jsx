6787797b0961fa421c6f3450a590584efcd1fcc7
Rez <rezhates@gmail.com>
2025-11-17 23:54:56 +0530

    #3 change threshold to change speed

--- src/component/ImgCursor.jsx
+++ src/component/ImgCursor.jsx
@@ -3,14 +3,27 @@ import gsap from "gsap";
 import React, { useRef } from "react";
 gsap.registerPlugin(useGSAP);
 
+const images =[
+    "/images/f3.jpg",
+    "/images/f4.jpg",
+    "/images/f5.jpg",
+    "/images/f6.jpg",
+    "/images/f7.jpg",
+    "/images/f8.jpg",
+    "/images/f3.jpg",
+]
+
 const ImgCursor = () => {
   const containerRef = useRef(null);
   const count = 6;
   const lastpos = useRef({ x: 0, y: 0 });
 
-  const threshold = 40; 
-  const imgIndex = useRef(0); 
-  const images = Array.from({ length: count }, (_, i) => `/images/f${i + 3}.jpg`);
+  const threshold = 60;
+  const imgIndex = useRef(0);
+  const images = Array.from(
+    { length: count },
+    (_, i) => `/images/f${i + 3}.jpg`
+  );
 
   useGSAP(() => {
     const createTrail = (x, y) => {
@@ -18,7 +31,7 @@ const ImgCursor = () => {
       if (!container) return;
 
       const img = document.createElement("img");
-      img.classList.add("image-trail"); 
+      img.classList.add("image-trail");
       img.src = images[imgIndex.current];
       container.appendChild(img);
 
@@ -43,7 +56,7 @@ const ImgCursor = () => {
         scale: 0.2,
         opacity: 0,
         duration: 1,
-        delay: 0.3,
+        delay: 0.5,
         ease: "power2.in",
         onComplete: () => img.remove(),
       });
