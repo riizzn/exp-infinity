a81b0399469e552ff855e523af2a045d705920ab
Rez <rezhates@gmail.com>
2026-01-15 01:39:47 +0530

    Add droping blocks animation using matter.js

--- src/App.jsx
+++ src/App.jsx
@@ -8,13 +8,15 @@ import Explosion from "./component/Explosion";
 import ReactLenis from "lenis/react";
 import Anim2 from "./revealAnimation/Anim2";
 import Flip from "./component/Flip";
+import Physics from "./component/Physics";
 
 const App = () => {
   return (
     <>
       <ReactLenis root />
       <div className="min-h-screen">
-     <Flip/>
+        <Physics/>
+    
       </div>
     </>
   );
